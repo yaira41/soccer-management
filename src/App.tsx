9cdb31aa00d5065a5c31cf6f5a7fd3f8649a5c04
Yair Avivi <yavivi441@gmail.com>
2024-12-03 22:49:54 +0200

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ImageSlider from "./components/imagesSlider/ImageSlider";
+import DebtChecker from "./components/debtChecker/DebtChecker";
+import "./App.css";
+
+const App: React.FC = () => (
+  <div className="container">
+    <DebtChecker />
+    <ImageSlider />
+  </div>
+);
+
+export default App;
