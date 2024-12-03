2fdf554bba04b8536a3c912c964b2e93a5da7751
Yair Avivi <yavivi441@gmail.com>
2024-12-03 23:10:37 +0200

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ImageSlider from "./components/imagesSlider/ImageSlider";
 import DebtChecker from "./components/debtChecker/DebtChecker";
 import "./App.css";
 
+//App
 const App: React.FC = () => (
   <div className="container">
     <DebtChecker />
